df69cf95e2eaa3bff50c2f12602ca337879a9ca9
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-12-16 12:04:42 +0100

    improve tokenization process

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,29 +1,31 @@
 import json
-from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from transformers import GPT2Tokenizer
+from concurrent.futures import ProcessPoolExecutor
 
+def tokenize_data(pair):
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    return tokenizer.encode_plus(pair[0], pair[1], truncation=True, max_length=512, padding='max_length', return_tensors='pt')
 
 def load_dataset(file_path):
-    questions = []
-    answers = []
+    pairs = []
     with open(file_path, 'r') as file:
         for line in file:
             data = json.loads(line)
-            questions.append(data['questionText'])
-            answers.append(data['answers'][0]['answerText'])
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    token_questions = [tokenizer.encode(q, truncation=True, max_length=512) for q in questions]
-    token_answers = [tokenizer.encode(a, truncation=True, max_length=512) for a in answers]
+            question = data['questionText']
+            answer = data['answers'][0]['answerText']
+            pairs.append((question, answer))
 
-    df = pd.DataFrame({'questions': token_questions, 'answers': token_answers})
-    return df
-                       
+    # Parallel tokenization
+    with ProcessPoolExecutor() as executor:
+        tokenized_pairs = list(executor.map(tokenize_data, pairs))
+
+    return pd.DataFrame({'encodings': tokenized_pairs})
 
 def split_dataset(df, test_size=0.1):
     train_df, test_df = train_test_split(df, test_size=test_size)
     return train_df, test_df
-    
 
 if __name__ == '__main__':
     file_path = 'test-qar_all.jsonl'
