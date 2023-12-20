eb43d098375b5f88f8b5664bc7bbd099f26456c6
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-12-20 21:35:41 +0100

    update

--- train.py
+++ train.py
@@ -1,54 +1,62 @@
-import argparse
-import pandas as pd
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from transformers import Trainer, TrainingArguments
 from model import load_model
-
+import pandas as pd
 
 class ChatbotDataset(Dataset):
-    def __init__(self, dataframe):
-        self.encodings = dataframe['encodings'].tolist()
+    def __init__(self, encodings):
+        self.encodings = encodings
 
     def __getitem__(self, idx):
-        item = self.encodings[idx]
-        item['labels'] = item['input_ids'].clone().detach()  # Copy of input_ids for labels
-        return {key: torch.tensor(val).cuda() for key, val in item.items()}  # Move to GPU
+        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
 
     def __len__(self):
-        return len(self.encodings)
+        return len(self.encodings['input_ids'])
+
 def train(model_name, train_dataset, test_dataset):
     model, tokenizer = load_model(model_name)
-    if torch.cuda.is_available():
-      model = model.cuda()
-    else:
-        print("CUDA is not available. Check your Colab runtime settings.")
+    
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+    
+    train_dataset = ChatbotDataset(train_dataset.to_dict('list'))
+    test_dataset = ChatbotDataset(test_dataset.to_dict('list'))
 
-    print(next(model.parameters()).device) 
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=4, pin_memory=True)
+
     training_args = TrainingArguments(
         output_dir='./model_save',
         num_train_epochs=3,
-        per_device_train_batch_size=2,
+        per_device_train_batch_size=8,  # Same as DataLoader batch size
         warmup_steps=500,
         weight_decay=0.01,
         logging_dir='./logs',
+        load_best_model_at_end=True,
+        report_to="none"    # Disable logging to WANDB
     )
+    
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset,
+        train_dataset=train_loader,  
+        eval_dataset=test_loader     
     )
+    
+    # Start the training
     trainer.train()
+    
+    # Evaluate the model with the test data after training
+    trainer.evaluate(eval_dataset=test_loader)
+
+    # Save the fine-tuned model
     model.save_pretrained('./model_save/' + model_name)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='gpt2', help='Model name (gpt2, bert)')
-    args = parser.parse_args()
-
-    train_df = pd.read_pickle('dataset/train_dataset.pkl')
-    test_df = pd.read_pickle('dataset/test_dataset.pkl')
-    train_dataset = ChatbotDataset(train_df)
-    test_dataset = ChatbotDataset(test_df)
-    train(args.model, train_dataset, test_dataset)
+    model_name = 'gpt2'  # Or use argparse to set this
+    train_df = pd.read_pickle('train_dataset.pkl')
+    test_df = pd.read_pickle('test_dataset.pkl')
+    
+    # Start training
+    train(model_name, train_df, test_df)
