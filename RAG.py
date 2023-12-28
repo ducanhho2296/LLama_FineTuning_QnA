5d8cd6e486b166a584ef5166801f3f9cfed5ce45
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-12-28 12:18:55 +0100

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -3,3 +3,12 @@ from transformers import RagTokenizer, RagRetriever, RagSequenceForGeneration
 
 tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-base")
 retriever = RagRetriever.from_pretrained("facebook/rag-token-base")
+
+model_path = "./model_save/"
+model = AutoModelForCausalLM.from_pretrained(model_path)
+
+#example
+querry = "What is langchain and explain it clearly with example."
+
+#retrieve relevant documents
+retrieved_docs = retriever.retrieve(querry)
