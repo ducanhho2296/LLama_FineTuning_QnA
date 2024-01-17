e517a0b39bc47466cfc3fea33b5bcfb83173567a
Duc Anh Ho <ducanh.ho2296@gmail.com>
2024-01-17 14:31:00 +0100

    create format_instruction()

--- finetune_llamas2.py
+++ finetune_llamas2.py
@@ -5,3 +5,19 @@ from trl import SFTTrainer
 from model import load_model
 
 
+#loading dataset
+def load_dataset(path):
+    """adding function to load dataset here"""
+    pass
+
+def format_instruction(sample):
+	return f"""### Instruction:
+Use the Input below to create an instruction, which could have been used to generate the input using an LLM.
+
+### Input:
+{sample['response']}
+
+### Response:
+{sample['instruction']}
+"""
+
