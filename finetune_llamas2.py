1ba3f788cf792a7fdbf648907b0557fa75faf04f
Duc Anh Ho <ducanh.ho2296@gmail.com>
2024-01-17 14:31:38 +0100

    create finetune function

--- finetune_llamas2.py
+++ finetune_llamas2.py
@@ -21,3 +21,29 @@ Use the Input below to create an instruction, which could have been used to gene
 {sample['instruction']}
 """
 
+def finetune_llama2():
+    #set up flash attention to speed up and reduce mem
+    flash_attention = False
+    #load model
+    model_id = "NouseResearch/Llama-2-7b-hf"
+
+    # setup QloRA 4-bit quantization with BitsAndBytesConfig
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True, 
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_use_double_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        quantization_config=bnb_config,
+        use_cache=False,
+        use_flash_attention_2=flash_attention,
+        device_map="auto"
+    )
+    model.config.pretraining_tp = 1
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
