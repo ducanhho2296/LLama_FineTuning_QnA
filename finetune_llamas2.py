dd33d95b197c8723ab9fcd5c75b96cec9e13665c
Duc Anh Ho <ducanh.ho2296@gmail.com>
2024-01-17 14:30:27 +0100

    Create finetune_llamas2.py

---
+++ finetune_llamas2.py
@@ -0,0 +1,7 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
+from trl import SFTTrainer
+from model import load_model
+
+
